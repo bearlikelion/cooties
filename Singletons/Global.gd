cc987cda1f2fbf65345c933029e0f1e4d7140064
Mark <markarneman@gmail.com>
2025-12-28 14:29:51 -0500

    Send player to server

--- Singletons/Global.gd
+++ Singletons/Global.gd
@@ -69,7 +69,15 @@ func _on_connected_to_server() -> void:
 			"score": 0
 		}
 
+	send_player_to_server.rpc_id(1, players[local_id])
+
+
+@rpc("any_peer", "call_remote", "reliable")
+func send_player_to_server(player: Dictionary) -> void:
 	if multiplayer.is_server():
+		var sender_id: int = multiplayer.get_remote_sender_id()
+		players[sender_id] = player
+
 		_sync_players_to_peer(players)
 
 
